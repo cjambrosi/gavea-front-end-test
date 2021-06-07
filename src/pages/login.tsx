241471484fce641a6b8886fc84cda380bee94258
Cristian J. Ambrosi <cristian.ambrosi@compasso.com.br>
2021-06-07 19:03:04 -0300

    feat(login page): page finished

---
+++ src/pages/login.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import { MainContent } from '../elements/MainContent'
+import { Footer } from '../components/Footer'
+import { SignIn } from '../components/SignIn'
+
+export default function Login() {
+  return (
+    <div className="teste">
+      <Head>
+        <title>Login - Gavea Marketplace</title>
+      </Head>
+      <MainContent>
+        <SignIn />
+      </MainContent>
+      <Footer
+        textInfo="Não tem uma conta?"
+        urlLing="/register"
+        textLink="Criar conta"
+      />
+    </div>
+  )
+}
