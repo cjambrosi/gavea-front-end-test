c0d8590d9f32151529a8f3fe794ac087ae764e50
Cristian J. Ambrosi <cristian.ambrosi@compasso.com.br>
2021-06-07 19:08:07 -0300

    feat(register page): page finished

---
+++ src/pages/register.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import { MainContent } from '../elements/MainContent'
+import { Footer } from '../components/Footer'
+import { SignUp } from '../components/SignUp'
+
+export default function Register() {
+  return (
+    <div className="teste">
+      <Head>
+        <title>Cadastro - Gavea Marketplace</title>
+      </Head>
+      <MainContent>
+        <SignUp />
+      </MainContent>
+      <Footer
+        textInfo="Já tem uma conta?"
+        urlLing="/login"
+        textLink="Faça o login"
+      />
+    </div>
+  )
+}
