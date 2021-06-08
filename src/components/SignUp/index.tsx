7539346e0bf0a577375d016c7bc99453b84752b4
Cristian J. Ambrosi <cristian.ambrosi@compasso.com.br>
2021-06-08 18:50:30 -0300

    feat(page and element): register function implemented

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,45 +1,76 @@
+import { useState, FormEvent } from 'react'
+import { useAuth } from '../../hooks/useAuth'
+
 import { Button } from '../../elements/Button'
 import { Input } from '../../elements/Input'
 import { Wrapper, Title, ContentForm } from './styles'
 
 export function SignUp() {
+  const { signUp } = useAuth()
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleCreateNewUser = async (event: FormEvent) => {
+    event.preventDefault()
+
+    signUp({ name, email, password })
+  }
+
   return (
     <Wrapper>
       <Title>Criar Conta</Title>
 
-      <ContentForm>
+      <ContentForm onSubmit={handleCreateNewUser}>
         <div className="input-block">
           <Input
             type="text"
+            name="name-field"
+            value={name}
             placeholder="Nome completo"
             leftIcon="icon-person"
+            onChange={(event) => setName(event.target.value)}
           />
         </div>
 
         <div className="input-block">
-          <Input type="email" placeholder="Email" leftIcon="icon-email" />
+          <Input
+            type="email"
+            name="email-field"
+            value={email}
+            placeholder="Email"
+            leftIcon="icon-email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </div>
 
         <div className="input-block">
           <Input
             type="password"
+            name="password-field"
+            value={password}
             placeholder="Senha"
             leftIcon="icon-password"
             rigthIcon={true}
+            onChange={(event) => setPassword(event.target.value)}
           />
         </div>
 
         <div className="input-block">
           <Input
             type="password"
+            name="confirm-passowrd-field"
+            value={password}
             placeholder="Confirmar Senha"
             leftIcon="icon-password"
             rigthIcon={true}
+            onChange={(event) => setPassword(event.target.value)}
           />
         </div>
 
         <Button
-          text="Entrar"
+          typeButton="submit"
+          text="Cadastrar"
           textColor="--white"
           backgroundColor="--orangey-600"
           borderColor="--orangey-600"
