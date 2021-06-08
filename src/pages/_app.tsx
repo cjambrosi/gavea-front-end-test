489fdd798dc0da5f9c22f0100f8b86397a4bd0ce
Cristian J. Ambrosi <cristian.ambrosi@compasso.com.br>
2021-06-08 14:52:05 -0300

    feat(page): page loading animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import { AppProps } from 'next/app'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 
 import { GlobalStyle } from '../styles/globals'
 import '/public/assets/icons/gavea-icons/style.css'
+import 'nprogress/nprogress.css'
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start()
+})
+
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
