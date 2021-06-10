a4288e83d0ba61080d8f08c3624e742d8faede89
Cristian J. Ambrosi <cristian.ambrosi@compasso.com.br>
2021-06-09 23:07:49 -0300

    refactor(element maincontent): removed commented code

--- src/elements/MainContent/index.tsx
+++ src/elements/MainContent/index.tsx
@@ -3,10 +3,8 @@ import { Main } from './styles'
 
 interface MainContentProps {
   children: ReactNode
-  // alignJustify: string
 }
 
 export function MainContent({ children }: MainContentProps) {
-  // return <Main alignJustify={alignJustify}>{children}</Main>
   return <Main>{children}</Main>
 }
